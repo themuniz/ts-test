1dedf31fd553cd3fa81a2cb11140ecf48d34cb7b
José Muñiz <jose.muniz@cuny.edu>
2023-09-14 10:58:27 -0400

    setup html templating

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,31 @@ import express, { Request, Response } from 'express'
 import { z } from 'zod'
 import 'dotenv/config'
 import * as sql from 'mssql'
+import nunjucks from 'nunjucks'
 
 const app = express()
 
 // Process .env config
 const envVariables = z.object({
-  PORT: z.string(),
+  PORT: z.coerce.number(),
   DB_DATABASE: z.string(),
   DB_SERVER: z.string(),
   DB_USER: z.string(),
   DB_PASSWORD: z.string(),
+  TMPL_NOCACHE: z.coerce.boolean(),
 })
 const env = envVariables.parse(process.env)
 
-// Express configs and middlewear
+// Express configs and middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('assets'))
+
+// Templating
+nunjucks.configure('views', {
+  express: app,
+  noCache: env.TMPL_NOCACHE,
+})
 
 // DB
 const dbConfig = {
@@ -37,8 +46,8 @@ const dbConfig = {
 // Routes
 app.get('/', async (req: Request, res: Response) => {
   await sql.connect(dbConfig)
-  const response = await sql.query`select * from uvw_person`
-  return res.send(`${JSON.stringify(response.recordset)}`)
+  const response = await sql.query`select * from uvw_person order by last_name`
+  return res.render('person.html', { data: response.recordset })
 })
 
 app.listen(env.PORT, () => {
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['views/**/*.{html,js}', 'node_modules/preline/dist/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: ["require('preline/plugin')"],
+}
