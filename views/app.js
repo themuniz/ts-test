f6435badd86ddbb014b7c1ca4178782434823947
José Muñiz <jose.muniz@cuny.edu>
2023-11-08 13:11:55 -0500

    Built login form
    - Built form to display when we either do not have a user in
      localstorage or when the token fails validation
    - Built error notifications for missing fields, server outages, or
      incorrect credentials

--- views/app.js
+++ views/app.js
@@ -18,11 +18,10 @@ document.addEventListener('alpine:init', () => {
     async init() {
       const user = localStorage.getItem('user')
       if (!user) {
-        this.messageBoxType = 'error'
-        this.messageBoxHtml =
-          '<p>You are not logged in. Please go to the <a class="text-blue-700" href="https://ofditprojects.sps.cuny.edu/" target="_blank">login</a> page, and then refresh this page after successfully logging in.</p><div><label for="username">Username:</label><input id="username" type="text" x-model="loginUsername"/><label for="password">Password:</label><input id="password" type="password" x-model="loginPassword"/><button @click="login">Login</button>'
+        this.setLoginForm()
       } else {
         this.user = JSON.parse(user)
+        this.messageBoxType = 'info'
         this.messageBoxHtml = 'Checking credentials'
         this.isLoggedIn = await this.checkToken(this.user.token)
       }
@@ -33,6 +32,11 @@ document.addEventListener('alpine:init', () => {
           `{"Authentication": "Bearer: ${this.user.token}"}`,
         )
       }
+    },
+    setLoginForm() {
+      this.messageBoxType = 'error'
+      this.messageBoxHtml =
+        '<p>You are not logged in. Please use the login form below to access this page.</p><div class="flex mt-3 mx-auto flex-col md:w-1/2"><label for="username">Username</label><input class="rounded p-1" id="username" type="text" x-model="loginUsername"/><label class="mt-2" for="password">Password</label><input id="password" class="rounded p-1" type="password" x-model="loginPassword" @keyup.enter="login"/><button class="self-end mt-2 p-2 border bg-blue-700 text-white rounded hover:bg-blue-500" @click="login">Login</button>'
     },
     async login() {
       try {
@@ -49,8 +53,29 @@ document.addEventListener('alpine:init', () => {
         this.sendNotification(
           `Greetings ${this.user.first_name}, you have successfully logged in.`,
         )
+        this.init()
       } catch (err) {
-        console.error(err)
+        if (err.response.status === 400) {
+          this.sendNotification(
+            'Please check that you have entered both your username & password.',
+            'error',
+            'Error',
+          )
+        }
+        if (err.response.status === 401) {
+          this.sendNotification(
+            'The login server rejected your credentials. Please try again. If you cannot login, please contact OFDIT.',
+            'error',
+            'Error',
+          )
+        }
+        if (err.response.status === 500) {
+          this.sendNotification(
+            'The login server is down. Please contact OFDIT if this continues.',
+            'error',
+            'Error',
+          )
+        }
       }
     },
     async checkToken(token) {
@@ -69,19 +94,23 @@ document.addEventListener('alpine:init', () => {
         )
         return true
       } catch (err) {
-        this.messageBoxType = 'error'
-        this.messageBoxHtml =
-          'Your credential check failed. Please go to the <a class="text-blue-500" href="https://ofditprojects.sps.cuny.edu/" target="_blank">Login</a> page, and then return to refresh this page after successfully logging in.'
+        this.setLoginForm()
       }
       return false
     },
-    async sendNotification(message) {
+    async sendNotification(
+      message,
+      messageType = 'success',
+      toastTitle = 'Success',
+    ) {
       this.toastMessage = message
-      this.toastType = 'success'
+      this.toastType = messageType
+      this.toastTitle = toastTitle
       this.showToast = true
       await new Promise((res) => setTimeout(res, 3000))
       this.showToast = false
       this.toastMessage = ''
+      this.toastTitle = ''
     },
     async copyToClipboard(value) {
       await navigator.clipboard.writeText(value)
